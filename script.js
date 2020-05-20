11e891503c412591bc6dbe642c07ec3eb83fd7c8
Laura MacKew <48992747+l-mack@users.noreply.github.com>
2020-05-20 18:35:25 -0400

    mapping out functions

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ const ethicalFashionApp = {};
 // collect user input 
 
 const searchInput = document.querySelector('.search');
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
+
+
+// display matches
+function displayMatches() {
+    console.log(this.value);
+}
 
 // compare user input to data array 
-    // if match -> display result
-    // if no match found -> dipslay error message
+    // find matches function 
+        // takes in wordToMatch and data array
+        //returns a filtered data array
+            // 
+            // if wordToMatch matches brand name in data array
+
+
 
     // display css styles (background color) depending on company rating (e.g. A+ rating = green background)
 
