0cf00e02b4fe52f566d4094d6fba0893c31a9bef
Laura MacKew <48992747+l-mack@users.noreply.github.com>
2020-05-20 01:04:00 -0400

    init commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// collect user input 
+
+// compare user input to json data 
+    // if match -> display result
+    // if no match found -> dipslay error message
+
+    // display css styles (background color) depending on company rating (e.g. A+ rating = green background)
+
