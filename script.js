f3a14691a2eb3b6231dbea3083bdef7ca605107e
Laura MacKew <48992747+l-mack@users.noreply.github.com>
2020-05-20 03:27:10 -0400

    form and function start

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+const ethicalFashionApp = {};
+
 // collect user input 
 
-// compare user input to json data 
+const searchInput = document.querySelector('.search');
+
+// compare user input to data array 
     // if match -> display result
     // if no match found -> dipslay error message
 
     // display css styles (background color) depending on company rating (e.g. A+ rating = green background)
 
+//stretch goals
+    // move data to DB and connect 
+    // show results as user types 
